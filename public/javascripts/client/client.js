ad7a365d4ccbcb1295e0c3049b48b3e90c5f150c
sartios <sartios@hotmail.com>
2016-08-25 17:08:54 +0300

    chat-app: add user name

--- public/javascripts/client/client.js
+++ public/javascripts/client/client.js
@@ -1,18 +1,40 @@
 var socket = io.connect('/');
 
+var name;
+
 socket.on('message', function(data){
   data = JSON.parse(data);
-  $('#messages').append('<div class="'+data.type+'">' + data.message + '</div>');
+  if(data.username){
+    if(data.username == name){
+      $('#messages').append('<div class="username"><b>me</b></div><div class="'+data.type+'">' + data.message + '</div>');
+    }else{
+      $('#messages').append('<div class="username"><b>'+data.username+'</b></div><div class="'+data.type+'">' + data.message + '</div>');
+    }
+
+  }else{
+      $('#messages').append('<div class="'+data.type+'">' + data.message + '</div>');
+  }
+
+});
+
+socket.on('name_set', function(data){
+  $('#nameform').hide();
+  $('#messages').append('<div class="systemMessage">'+ 'Hello ' + data.username +'</div>');
+  $('#send').click(function(){
+    var data = {
+      message: $('#message').val(),
+      type: 'userMessage',
+      username: name
+    };
+    socket.send(JSON.stringify(data));
+    $('#message').val('');
+  });
 });
 
 // On document.ready
 $(function(){
-  $('#send').click(function(){
-    var data = {
-      message: $('#message').val(),
-      type: 'userMessage'
-    };
-    socket.send(JSON.stringify(data));
-    $('#message').val('');
+  $('#setname').click(function(){
+    name = $('#nickname').val();
+    socket.emit("set_name", {username: name});
   });
 });
