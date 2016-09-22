eb7eaf0151f81e77a12fb487cace8d2490e722e9
sartios <sartios@hotmail.com>
2016-09-22 18:48:28 +0300

    implement simple one user chat

--- src/index.js
+++ src/index.js
@@ -3,9 +3,34 @@ var ReactDOM = require('react-dom');
 var io = require('socket.io/node_modules/socket.io-client');
 
 var App = React.createClass({
+	getInitialState: function(){
+		return {
+			messages: []
+		}
+	},
+	handleSubmit: function(e){
+		var body = e.target.value;
+		if(e.keyCode === 13 && body){
+			var message = {
+				body,
+				from: 'Me'
+			}
+			var messages = this.state.messages;
+			messages.push(message);
+			this.setState({
+				messages: messages
+			});
+			e.target.value = '';
+		}
+	},
 	render: function(){
+		var messages = this.state.messages.map(function(message, index){
+			return <li key={index}><b>{message.from}:</b>{message.body}</li>
+		})
 		return (<div>
-				Hello World!
+				<h1>Hello World</h1>
+				<input type='text' placeholder='Enter a message...' onKeyUp={this.handleSubmit}/>
+				{messages}
 			</div>);
 	}
 });
