9cd8ed0898854c8bea4031cc6a7dc38010d55982
sartios <sartios@hotmail.com>
2016-09-29 16:39:13 +0300

    user-actions: add disconnect action

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -6,11 +6,19 @@ var UserActions = {
 	init: function(){
 		var self = this;
 		SocketHandler.addEventListener('user_connected', self.userConnected);
+		SocketHandler.addEventListener('user_disconnected', self.userDisconnected);
 	},
 	userConnected: function(user){
 		console.log('userConnected ' + user.id);
 		AppDispatcher.dispatch({
 			actionType: UserConstants.USER_CONNECT,
+			user: user
+		});
+	},
+	userDisconnected: function(user){
+		console.log('userDisconnected: ' + user.id);
+		AppDispatcher.dispatch({
+			actionType: UserConstants.USER_DISCONNECT,
 			user: user
 		});
 	}
