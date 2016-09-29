a1db89c8b0d81a00b1aec690031bcf652dbe872b
sartios <sartios@hotmail.com>
2016-09-29 15:50:05 +0300

    socket-server: broadcast event on new connection, keep track of connections and add disconnect event with handler

--- index.js
+++ index.js
@@ -33,13 +33,29 @@ app.post('/', function(req, res){
 });
 
 
+var connections = [];
+
 io.on('connection', function(socket){
+
+	connections.push(socket);
+	
+	socket.broadcast.emit('user_connected',{
+		id: socket.id.slice(8)
+	});
+
 	socket.on('message', function(body){
+		console.log('message received');
 		socket.broadcast.emit('message', {
 			body: body,
 			from: socket.id.slice(8)
 		});
 	});
+
+	socket.on('disconnect', function(data){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockects connected', connections.length);
+  });
+
 });
 
 server.listen(3001);
