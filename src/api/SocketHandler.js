e4dc2dca721d7104a3a2e02461172c93bba6446a
sartios <sartios@hotmail.com>
2016-09-29 15:48:02 +0300

    refactor socket handler

--- src/api/SocketHandler.js
+++ src/api/SocketHandler.js
@@ -2,13 +2,19 @@ var io = require('socket.io/node_modules/socket.io-client');
 
 var SocketHandler = {
 	addEventListener: function(event, callback){
-		this.socket.on(event, callback);
+		this.getSocket().on(event, callback);
 	},
 	emit: function(event, message){
-		this.socket.emit(event, message);
+		this.getSocket().emit(event, message);
 	},
 	connect: function(room){
 		this.socket = io(room);
+	},
+	getSocket: function(){
+		if(this.socket === undefined){
+			this.socket = io('/');		
+		}
+		return this.socket;
 	}
 };
 
