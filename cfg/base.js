ff5a063be73293dd45d66a85869638a9dde3092a
sartios <sartios@hotmail.com>
2016-10-04 18:06:53 +0300

    webpack: add basic configuration

---
+++ cfg/base.js
@@ -0,0 +1,32 @@
+'use strict';
+var path = require('path');
+var defaultSettings = require('./defaults');
+
+module.exports = {
+  port: defaultSettings.port,
+  debug: true,
+  devtool: 'source-maps',
+  output:{
+    path: path.join(__dirname, '/../public/js'),
+    filename: 'bundle.js',
+    publicPath: defaultSettings.publicPath
+  },
+  devServer: {
+    contentBase: './src/',
+    historyApiFallback: true,
+    hot: true,
+    port: defaultSettings.port,
+    publicPath: defaultSettings.publicPath,
+    noInfo: false
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      actions: '${defaultSettings.srcPath}/actions/',
+      components: '${defaultSettings.srcPath}/components/',
+      stores: '${defaultSettings.srcPath}/stores/',
+      styles: '${defaultSettings.srcPath}/styles/'
+    }
+  },
+  module: {}
+};
