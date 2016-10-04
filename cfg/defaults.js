b2f563a3100f99e2ed7df5c7816a8cb04db4de05
sartios <sartios@hotmail.com>
2016-10-04 17:00:29 +0300

    webpack: add default configuration

---
+++ cfg/defaults.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var path = require('path');
+var srcPath = path.join(__dirname, '/../src');
+var dfltPort = 8000;
+
+/**
+ * Get the default modules object for webpack
+ */
+ function getDefaultModules(){
+   return {
+     preLoaders:[
+       {
+         test: /\.(js|jsx)$/,
+         include: srcPath,
+         loader: 'eslint-loader'
+       }
+     ],
+     loaders: [
+       {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader'
+       },
+       {
+          test: /\.less/,
+          loader: 'style-loader!css-loader!less-loader'
+       },
+      {
+        test: /\.(png|jpg|gif|woff|woff2)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.(mp4|ogg|svg)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(eot|ttf)/,
+        loader: 'file-loader'
+      }
+     ]
+  };
+ }
+
+ module.exports = {
+   srcPath: srcPath,
+   publicPath: '/assets/',
+   port: dfltPort,
+   getDefaultModules: getDefaultModules
+ };
