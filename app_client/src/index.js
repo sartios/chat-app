b231ba8ce39f701c520f6934193fb0cf046d8196
sartios <sartios@hotmail.com>
2016-10-05 16:17:07 +0300

    app-client: add font style

--- app_client/src/index.js
+++ app_client/src/index.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ChatRoom = require('./components/ChatRoom');
 require('bootstrap/less/bootstrap.less');
+require('./styles/style.less');
 
 var App = React.createClass({
 	render: function(){
